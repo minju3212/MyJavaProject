3856b927265bd86a46c41f649cb3eac4871e614e
minju3212 <smj3mjs@gmail.com>
2025-04-23 11:24:00 +0900

    accounttest add

---
+++ src/workshop/account/test/AccountTest.java
@@ -0,0 +1,10 @@
+package workshop.account.test;
+
+import workshop.account.entity.Account;
+
+public class AccountTest {
+	public static void main(String[] args) {
+		//Account °´Ã¼
+		Account account = new Account();
+	}
+}
